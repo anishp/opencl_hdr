81a11c86c544315d00cacec4cf8f43fe545a67aa
Anish Pednekar <pednekar.anish@gmail.com>
2014-07-05 18:04:14 +0530

    Initial Commit

---
+++ opencl_hdr/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  opencl_hdr
+//
+//  Created by Anish Pednekar on 7/5/14.
+//  Copyright (c) 2014 Anish Pednekar. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
